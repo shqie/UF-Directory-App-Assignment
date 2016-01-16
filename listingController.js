900760d1359754fa41b33a74c729616bda140d23
shqie <visishqiponja@yahoo.com>
2016-01-15 23:21:31 -0500

    Finish add option, and styling

--- listingController.js
+++ listingController.js
@@ -7,7 +7,10 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
+    $scope.addListing = function() {
+      $scope.listings.push($scope.newListing);
+      $scope.newListing = '';
+    };
     $scope.deleteListing = function(index) {
        $scope.listings.splice(index, 1);
     };
